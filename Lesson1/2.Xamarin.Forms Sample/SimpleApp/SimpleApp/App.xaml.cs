1dab64db829f6b2b6d7b09ec5a569851cf9c05cb
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-01-10 03:46:11 +0000

    Added xamarin.forms projects for lesson1

---
+++ Lesson1/2.Xamarin.Forms Sample/SimpleApp/SimpleApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+
+namespace SimpleApp
+{
+    public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new NavigationPage(new Styles.MainPage())
+            {
+                BarBackgroundColor = Color.AntiqueWhite
+            };
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ Lesson1/2.Xamarin.Forms Sample/SimpleApp/SimpleApp/NoStyles/OouiSamplePage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SimpleApp.Styles.NoStyles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class OouiSamplePage : ContentPage
+	{
+		public OouiSamplePage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
